49670d085982ae6c95af9eae32db249adeae780c
MSDOMAIN1\agaikw2 <ajay.gaikwad@morningstar.com>
2025-04-18 17:45:01 +0530

    initial message

---
+++ assets/js/menu.js
@@ -0,0 +1,1 @@
+if($(".smart-scroll").length>0){var l=0;$(window).on("scroll",function(){(scroll_top=$(this).scrollTop())<l?$(".smart-scroll").removeClass("scrolled-down").addClass("scrolled-up"):$(".smart-scroll").removeClass("scrolled-up").addClass("scrolled-down"),l=scroll_top})}
